c4c3887266d72990070a2c411d7245a274ad4045
h187455 <187455@stud.hvl.no>
2025-03-14 00:16:35 +0100

    z

--- demo/src/TabellMengde.java
+++ demo/src/TabellMengde.java
@@ -1,17 +1,19 @@
 public class TabellMengde<T> implements MengdeADT<T> {
-    private int size = 0;
+    private int size;
     private T[] elementsCopy;
     private T[] elements;
 
     @SuppressWarnings("unchecked")
     public TabellMengde() {
         this.elements = (T[]) new Object[10];
+        this.size = 0;
     }
 
     public boolean erTom(){
         return size == 0;
     }
 
+    @SuppressWarnings("unchecked")
     public void leggTil(T element) {
         boolean check = true;
         for (int i = 0; i < size; i++) {
@@ -21,7 +23,7 @@ public class TabellMengde<T> implements MengdeADT<T> {
         }
         
         if (size == elements.length && check) {
-            @SuppressWarnings("unchecked")
+            
             this.elementsCopy = (T[]) new Object[size*2];
             for (int i = 0; i < size; i++) {
                 elementsCopy[i] = elements[i]; 
@@ -44,18 +46,34 @@ public class TabellMengde<T> implements MengdeADT<T> {
             }
         }
         return null;
+
     }
 
     public int antallElementer() {
         return size;
     }
-
+    @SuppressWarnings("unchecked")
     public T[] tilTabell(){
-        @SuppressWarnings("unchecked")
+        
         T[] arr = (T[]) new Object[size];
         for (int i = 0; i < size; i++) {
             arr[i] = elements[i];
         }
         return arr;
+    }
+
+    public void leggTilAlleFra(MengdeADT<T> annenMengde){
+        T[] newElements = annenMengde.tilTabell();
+        for (int i = 0; i < newElements.length; i++) {
+            this.leggTil(newElements[i]);
+        }
+    }
+
+    public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
+        T[] newElements = annenMengde.tilTabell();
+        for (int i = 0; i < newElements.length; i++) {
+            this.fjern(newElements[i]);
+        }
+        
     }
 }
