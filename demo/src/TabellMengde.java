9d0245ea57a6c567ddb9d5622b185de5ee0166c2
h187455 <187455@stud.hvl.no>
2025-03-13 22:11:09 +0100

    d

--- demo/src/TabellMengde.java
+++ demo/src/TabellMengde.java
@@ -1,3 +1,21 @@
-public class TabellMengde implements MengdeADT{
+public class TabellMengde<T> implements MengdeADT<T> {
+    private int size = 0;
+    private T[] elements;
+
+    public boolean erTom(){
+        return size == 0;
+    }
+    
+    public void leggTil(T element) {
+        boolean check = true;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element) {
+                check = false;
+            }
+        }
+        if (check) {
+            elements[size++] = element;
+        }
+    }
 
 }
