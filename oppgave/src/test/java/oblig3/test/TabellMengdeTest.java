0a007ddbb6cabd0272082af2555b7cab0690532b
h187455 <187455@stud.hvl.no>
2025-03-20 14:29:55 +0100

    yeye

---
+++ oppgave/src/test/java/oblig3/test/TabellMengdeTest.java
@@ -0,0 +1,11 @@
+package oblig3.test;
+
+import oblig3.MengdeADT;
+import oblig3.TabellMengde;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TabellMengdeTest {
+
+}
